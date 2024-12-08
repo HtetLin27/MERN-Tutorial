6bc42d4967b406ec15f57ed439a1a6a8b1a25f5f
htetlynn <htetlynnko27@gmail.com>
2024-12-09 00:50:38 +0700

    backend CRUD setup

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
-app.post("/products", (req, res) => {
-  res.send("Server is ready");
-});
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => {
   connectDB();
